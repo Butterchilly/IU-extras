33eaa589529a8ea4f9a83d0b2ace34f168fc57aa
BBC-Scripts <88847062+Butterchilly@users.noreply.github.com>
2023-04-27 14:33:22 +0530

    small fix and update in fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,11 +3,12 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 "yes"
 
-description 'IU-extras Made By Butter'
+description 'IU-extras Made By Butter Chilly#0311'
 
 client_scripts{
     'client/Camforce.lua',
     'client/Bunnyhop.lua',
+    'client/Headshot.lua',
 }
 
 shared_scripts{
